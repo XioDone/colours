3e69fe41d3805e820c19c971d4ff33f5f68827e5
Doxo <gentleDone@outlook.com>
2023-11-18 22:03:06 +0800

    chore: update deps & eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ const config = antfu(
     rules: {
       'curly': ['error', 'all'],
       'no-console': 'off',
-      'arrow-parens': ['error', 'as-needed'],
+      // 'arrow-parens': ['error', 'as-needed'],
       'array-callback-return': 0,
       'prefer-promise-reject-errors': ['error', { allowEmptyReject: true }],
     },
@@ -21,11 +21,12 @@ const config = antfu(
     rules: {
       'style/brace-style': ['error', '1tbs'],
       'style/quotes': ['error', 'single', { avoidEscape: true }], // 单引号，允许反义
+      'style/arrow-parens': ['error', 'as-needed'],
     },
   },
   {
     plugins: {
-      ts: pluginTs,
+      // ts: pluginTs,
     },
     rules: {
       'ts/ban-types': [2, { types: { Function: false } }],
@@ -38,16 +39,25 @@ const config = antfu(
   },
   {
     plugins: {
-      vue: pluginVue,
+      // vue: pluginVue,
     },
     rules: {
       // 'vue/attribute-hyphenation': ['warn', 'never', { ignore: ['custom-prop'] }], // props attrs - disabled
       'vue/v-on-event-hyphenation': ['warn', 'never', { ignore: ['custom-event'] }], // v-on
+      'vue/component-name-in-template-casing': [
+        'error',
+        'PascalCase',
+        {
+          registeredComponentsOnly: false,
+          ignores: [],
+        },
+      ],
     },
   },
   {
     rules: {
       'jsdoc/no-defaults': 'off',
+      'jsdoc/require-returns-description': 'off',
     },
   },
   {
